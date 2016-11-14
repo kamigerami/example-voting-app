68050114c889f00a52d5769a850f6096c8211ce8
Kamjar Gerami <kami.gerami@gmail.com>
2016-11-14 18:19:17 +0100

    Changed ordering of vote-app.yaml services and added env variables for connections to db

--- vote/app.py
+++ vote/app.py
@@ -8,12 +8,13 @@ import json
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
+database = os.getenv('REDIS_SERVICE_HOST', "redis")
 
 app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host=database, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
